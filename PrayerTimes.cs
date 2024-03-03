5e027fa0f320c044cb45f793267c3837fa395a3c
noranaziz <noranaziz18@icloud.com>
2024-03-03 13:26:07 -0500

    added Newtonsoft.Json to parse result

--- PrayerTimes.cs
+++ PrayerTimes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http; // for http requests
 using System.Threading.Tasks; // for async programming
+using Newtonsoft.Json; // for parsing json
 
 class PrayerTimes {
     static async Task Main() {
